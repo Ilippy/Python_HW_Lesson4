f5c55abd4d4dc58e46ed867178f98c14b0355873
Ilippy <Ilippyk@gmail.com>
2023-02-28 18:54:34 +0300

    added HW + class tasks

---
+++ Task_27.py
@@ -0,0 +1,21 @@
+# Задача №27. Решение в группах
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+# Input: She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells
+# Output: 13
+# from string import punctuation
+
+#list(input("Введите предлжение через пробел").split())
+d =set("""She sells sea shells on the sea shore The shells
+that she sells are sea shells I'm sure.So if she sells sea
+shells on the sea shore I'm sure that the shells are sea
+shore shells""".replace("."," ").lower().split())
+
+
+print(sorted(d))
